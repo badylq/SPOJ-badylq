d2a6826312a93cfbddee42b2f1826d8c13810dc2
Krzysztof Zemlik <badylq@outlook.com>
2015-11-06 11:49:33 +0100

    FCTRL3

---
+++ FCTRL3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FCTRL3
+{
+	class Program
+	{
+		static private int Factorial(int number)
+		{
+			int factorial = 1;
+			for (int i = 1; i <= number; i++)
+			{
+				factorial *= i;
+			}
+			return factorial;
+		}
+		static void Main(string[] args)
+		{
+			int n, D;
+			n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				D = int.Parse(Console.ReadLine());
+				if (D <= 1)
+				{
+					Console.WriteLine("0 1");
+				}
+				else
+				{
+					if (D > 10)
+					{
+						Console.WriteLine("0 0");
+					}
+					else
+					{
+						string number = Factorial(D).ToString();
+						if (number.Length == 1)
+						{
+							Console.Write("0 ");
+						}
+						else
+						{
+							Console.Write(number[number.Length - 2] + " ");
+						}
+						Console.WriteLine(number[number.Length - 1]);
+					}
+				}
+			}
+		}
+	}
+}
