fb01e6a35f9ecefebd4f8530b14ab0300a686faf
Krzysztof Zemlik <badylq@outlook.com>
2015-11-09 14:10:28 +0100

    FLAMASTE

---
+++ FLAMASTE/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLAMASTE
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n;
+			string word;
+			n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				word = Console.ReadLine();
+				string newWord = "";
+				char c = word[0];
+				int counter = 0;
+				for (int j = 0; j < word.Length; j++)
+				{
+					if (c != word[j])
+					{
+						newWord += c;
+						if (counter == 2)
+						{
+							newWord += c;
+						}
+						else
+						{
+							if (counter > 2)
+							{
+								newWord += counter;
+							}
+						}
+						c = word[j];
+						counter = 1;
+					}
+					else
+					{
+						counter++;
+					}
+				}
+				newWord += c;
+				if (counter == 2)
+				{
+					newWord += c;
+				}
+				else
+				{
+					if (counter > 2)
+					{
+						newWord += counter;
+					}
+				}
+				Console.WriteLine(newWord);
+			}
+		}
+	}
+}
---
+++ PA05_POT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PA05_POT
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n, a, b;
+			int[,] powrs = new int[,]
+			{
+				{0, 0, 0, 0}, {1, 1, 1, 1}, {6, 2, 4, 8}, {1, 3, 9, 7}, {6, 4, 6, 4},
+				{5, 5, 5, 5}, {6, 6, 6, 6}, {6, 8, 4, 2}, {1, 9, 1, 9}
+			};
+			n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] numbers = inp.Split(' ');
+				a = int.Parse(numbers[0]);
+				b = int.Parse(numbers[1]);
+				int pow;
+				if (b == 0)
+				{
+					pow = 1;
+				}
+				else
+				{
+					a = a%10;
+					int x = b%4;
+					pow = powrs[a, x];
+				}
+				Console.WriteLine(pow);
+			}
+		}
+	}
+}
