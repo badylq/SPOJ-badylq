7b480e2f094002a6d4d6bcff4ce02e5d2d57a494
Krzysztof Zemlik <badylq@outlook.com>
2015-11-10 17:27:18 +0100

    PP0504D

---
+++ PP0504D/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace PP0504D
+{
+	class Program
+	{
+		private static void printFloat(float number)
+		{
+			byte[] bytes = BitConverter.GetBytes(number);
+			Array.Reverse(bytes);
+			StringBuilder hex = new StringBuilder(bytes.Length * 2);
+			foreach (byte b in bytes)
+			{
+				if (b < 16)
+				{
+					hex.AppendFormat("{0:x1}", b);
+				}
+				else
+				{
+					hex.AppendFormat("{0:x2}", b);
+				}
+				hex.Append(" ");
+			}
+			Console.WriteLine(hex);
+		}
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				float num = float.Parse(Console.ReadLine());
+				printFloat(num);
+				
+			}
+		}
+	}
+}
