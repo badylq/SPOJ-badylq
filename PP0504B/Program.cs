ca29bf85bfed2ead463654e5cd8ed465ec68e368
Krzysztof Zemlik <badylq@outlook.com>
2015-11-10 16:22:31 +0100

    PP0504B

---
+++ PP0504B/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PP0504B
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int len;
+			string output;
+			for (int i = 0; i < n; i++)
+			{
+				string[] str = Console.ReadLine().Split(' ');
+				if (str[0].Length > str[1].Length)
+				{
+					len = str[1].Length;
+				}
+				else
+				{
+					len = str[0].Length;
+				}
+				output = "";
+				for (int j = 0; j < len; j++)
+				{
+					output += str[0][j];
+					output += str[1][j];
+				}
+				Console.WriteLine(output);
+			}
+		}
+	}
+}
