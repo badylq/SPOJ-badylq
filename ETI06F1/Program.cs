c74deb8f1a763edb283b8e5f5b1ffaa3373d18d4
Krzysztof Zemlik <badylq@outlook.com>
2015-11-10 16:14:59 +0100

    ETI06F1

---
+++ ETI06F1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ETI06F1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			const double PI = 3.141592654;
+            double r, d;
+			string input = Console.ReadLine();
+			string[] values = input.Split(' ');
+			r = double.Parse(values[0]);
+			d = double.Parse(values[1]);
+			double rr = Math.Sqrt(r*r - (0.5*d)*(0.5*d));
+			double cArea = PI*(rr*rr);
+			Console.WriteLine(cArea);
+		}
+	}
+}
