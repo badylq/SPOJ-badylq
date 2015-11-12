42d9eed17e27eef8731d2985f6bfd548fff6d10a
Krzysztof Zemlik <badylq@outlook.com>
2015-11-12 17:31:48 +0100

    FANGEN simpler solution

--- FANGEN/Program.cs
+++ FANGEN/Program.cs
@@ -1,94 +1,23 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FANGEN
 {
 	class Program
 	{
-		private static void starsInCorners(ref char[,] matrix, int x, int n)
+		static private char swap(char c)
 		{
-			matrix[x, x] = '*';
-			matrix[x, x + 2*n - 1] = '*';
-			matrix[x + 2*n - 1, x] = '*';
-			matrix[x + 2*n - 1, x + 2*n - 1] = '*';
-		}
-
-		private static void fan(ref char[,] matrix, int x, int n, bool right)
-		{
-			char c1;
-			char c2;
-			if (right)
-			{
-				c1 = '*';
-				c2 = '.';
-			}
+			if (c == '*')
+				return '.';
 			else
-			{
-				c1 = '.';
-				c2 = '*';
-			}
-			int stars = 0;
-			for (int i = 0; i < 2*n - 1; i++)
-			{
-				if (stars < n)
-				{
-					matrix[x, x + i] = c1;
-					stars++;
-				}
-				else
-				{
-					matrix[x, x + i] = c2;
-				}
-			}
-
-			stars = 0;
-			for (int i = 0; i < 2 * n - 1; i++)
-			{
-				if (stars < n)
-				{
-					matrix[x + i, x + 2*n - 1] = c1;
-					stars++;
-				}
-				else
-				{
-					matrix[x + i, x + 2*n - 1] = c2;
-				}
-			}
-
-			stars = 0;
-			for (int i = 0; i < 2 * n; i++)
-			{
-				if (stars < n)
-				{
-					matrix[x + 2 * n - 1, x + i] = c2;
-					stars++;
-				}
-				else
-				{
-					matrix[x + 2 * n - 1, x + i] = c1;
-				}
-			}
-
-			stars = 0;
-			for (int i = 1; i < 2 * n; i++)
-			{
-				if (stars < n-1)
-				{
-					matrix[x + i, x] = c2;
-					stars++;
-				}
-				else
-				{
-					matrix[x + i, x] = c1;
-				}
-			}
+				return '*';
 		}
-
+		
 		static void Main(string[] args)
 		{
 			int r = int.Parse(Console.ReadLine());
             do
 			{
-				int n = 1;
 				bool right;
 				if (r<0)
 				{
@@ -99,27 +28,79 @@ namespace FANGEN
 				{
 					right = false;
 				}
-				char[,] matrix = new char[2*r,2*r];
-				int x = r - 1;
-				for (int i = 0; i < r; i++)
+
+				int a,b,c,d;
+				char[][] str = new char[r][];
+				char c1;
+				char c2;
+				int mod = 1;
+				if (right)
 				{
-					fan(ref matrix, x, n, right);
-					if (!right)
-					{
-						starsInCorners(ref matrix, x, n);
-					}
-					x--;
-					n++;
+					c1 = '*';
+					c2 = '.';
+					a = 0;
+					b = r;
+					c = r - 1;
+					d = 1;
+				}
+				else
+				{
+					c1 = '.';
+					c2 = '*';
+					a = 1;
+					b = r - 1;
+					c = r;
+					d = 0;
 				}
 
-				for (int i = 0; i < 2*r; i++)
+				for (int j = 0; j < r; j++)
+				{
+					int ii = 0;
+					str[j] = new char[2*r];
+					for (int i = 0; i < a; i++)
+					{
+						str[j][ii] = c2;
+						ii++;
+					}
+					for (int i = 0; i < b; i++)
+					{
+						str[j][ii] = c1;
+						ii++;
+					}
+					for (int i = 0; i < c; i++)
+					{
+						str[j][ii] = c2;
+						ii++;
+					}
+					for (int i = 0; i < d; i++)
+					{
+						str[j][ii] = c1;
+						ii++;
+					}
+
+					a += mod;
+					b -= mod;
+					c -= mod;
+					d += mod;
+				}
+				for (int i = 0; i < r; i++)
 				{
 					for (int j = 0; j < 2*r; j++)
 					{
-						Console.Write(matrix[i,j]);
+						Console.Write(str[i][j]);
 					}
-					Console.Write('\n');
+					Console.WriteLine();
 				}
+				
+				for (int i = r-1; i >= 0; i--)
+				{
+					for (int j = 2*r - 1; j >=0; j--)
+					{
+						Console.Write(str[i][j]);
+					}
+					Console.WriteLine();
+				}
+
 				Console.WriteLine();
 				r = int.Parse(Console.ReadLine());
 			} while (r != 0);
