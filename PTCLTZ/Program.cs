32fb2bb0888e5c35dea58ecc083190d0e4853542
Krzysztof Zemlik <badylq@outlook.com>
2015-11-12 13:38:18 +0100

    PTCLTZ

---
+++ PTCLTZ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PTCLTZ
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				int n = 0;
+				int x = int.Parse(Console.ReadLine());
+				while (x != 1)
+				{
+					if (x%2 == 0)
+					{
+						x = x/2;
+					}
+					else
+					{
+						x = 3*x + 1;
+					}
+					n++;
+				}
+				Console.WriteLine(n);
+			}
+		}
+	}
+}
---
+++ PTCLTZ/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PTCLTZ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PTCLTZ")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bfa4fba-a0bc-476e-be92-ffb8cbed98ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
