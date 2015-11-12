1f90108b9f001d697b52075b7dbb4810dc63cb33
Krzysztof Zemlik <badylq@outlook.com>
2015-11-12 13:50:01 +0100

    PTEST

---
+++ PTEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PTEST
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine((int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine())));
+		}
+	}
+}
