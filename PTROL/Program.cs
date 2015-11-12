683ce9e0cefcf8457e2f36b5d5b6b7cdd24d4a90
Krzysztof Zemlik <badylq@outlook.com>
2015-11-12 13:45:30 +0100

    PTROL

---
+++ PTROL/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PTROL
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] data = inp.Split(' ');
+				int n = int.Parse(data[0]);
+				for (int j = 2; j < n+1; j++)
+				{
+					Console.Write(data[j] + " ");
+				}
+				Console.WriteLine(data[1]);
+			}
+		}
+	}
+}
