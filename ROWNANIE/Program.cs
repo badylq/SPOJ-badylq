f2145acdd4cf8eefe5d5e62772c6323eb106e88a
Krzysztof Zemlik <badylq@outlook.com>
2015-11-13 08:09:16 +0100

    ROWNANIE

---
+++ ROWNANIE/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ROWNANIE
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			double a, b, c;
+			double delta;
+			var end = false;
+			do
+			{
+				inp = Console.ReadLine();
+				if (String.IsNullOrEmpty(inp))
+				{
+					end = true;
+				}
+				else
+				{
+					string[] data = inp.Split(' ');
+					a = double.Parse(data[0]);
+					b = double.Parse(data[1]);
+					c = double.Parse(data[2]);
+					delta = b*b - 4*a*c;
+					if (delta >= 0)
+					{
+						if (delta > 0)
+						{
+							Console.WriteLine("2");
+						}
+						else
+						{
+							Console.WriteLine("1");
+						}
+					}
+					else
+					{
+						Console.WriteLine("0");
+					}
+				}
+			} while (!end);
+		}
+	}
+}
