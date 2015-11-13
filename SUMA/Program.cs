6a8f9ec0a4a70ef34cd6663b0aa64dd5dc8d3930
Krzysztof Zemlik <badylq@outlook.com>
2015-11-13 07:56:55 +0100

    SUMA

---
+++ SUMA/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SUMA
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			int result = 0;
+			bool end = false;
+			do
+			{
+				inp = Console.ReadLine();
+				if (!String.IsNullOrEmpty(inp))
+				{
+					result += int.Parse(inp);
+					Console.WriteLine(result);
+				}
+				else
+				{
+					end = true;
+				}
+			} while (!end);
+		}
+	}
+}
