fa809e4ff79c1481f8fb1ce3ce44fc6819c1d3e0
Krzysztof Zemlik <badylq@outlook.com>
2015-11-14 16:28:07 +0100

    CALC

---
+++ CALC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CALC
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			do
+			{
+				inp = Console.ReadLine();
+				if (!string.IsNullOrEmpty(inp))
+				{
+					string[] data = inp.Split(' ');
+					if (data[0] == "+")
+					{
+						Console.WriteLine(int.Parse(data[1]) + int.Parse(data[2]));
+					}
+					if (data[0] == "-")
+					{
+						Console.WriteLine(int.Parse(data[1]) - int.Parse(data[2]));
+					}
+					if (data[0] == "*")
+					{
+						Console.WriteLine(int.Parse(data[1]) * int.Parse(data[2]));
+					}
+					if (data[0] == "/")
+					{
+						Console.WriteLine(int.Parse(data[1]) / int.Parse(data[2]));
+					}
+					if (data[0] == "%")
+					{
+						Console.WriteLine(int.Parse(data[1]) % int.Parse(data[2]));
+					}
+				}
+			} while (!string.IsNullOrEmpty(inp));
+		}
+	}
+}
