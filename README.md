c5ca3f151c7df71c9d2256eda3cdefa78ad3cc00
Krzysztof Zemlik <badylq@outlook.com>
2015-11-14 16:23:28 +0100

    STOS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SPOJ-badylq
 Problems from http://pl.spoj.com/
+All problems are from polish version fo SPOJ, and some projects can have polish naming in code.
---
+++ STOS/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace STOS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int size = 0;
+			int[] stack = new int[10];
+			string inp;
+			do
+			{
+				inp = Console.ReadLine();
+				if (!string.IsNullOrEmpty(inp))
+				{
+					if (inp == "+")
+					{
+						if (size < 10)
+						{
+							stack[size] = int.Parse(Console.ReadLine());
+							size++;
+							Console.WriteLine(":)");
+						}
+						else
+						{
+							Console.WriteLine(":(");
+						}
+					}
+					else
+					{
+						if (inp == "-")
+						{
+							if (size > 0)
+							{
+								size--;
+								Console.WriteLine(stack[size]);
+							}
+							else
+							{
+								Console.WriteLine(":(");
+							}
+						}
+					}
+				}
+			} while (!string.IsNullOrEmpty(inp));
+		}
+	}
+}
