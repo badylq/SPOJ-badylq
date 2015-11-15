7b5fa49750f83af13006f2586687d7fcb4140472
Krzysztof Zemlik <badylq@outlook.com>
2015-11-15 19:45:19 +0100

    PP0601A2

---
+++ PP0601A2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PP0601A2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int counter = 0;
+			bool checker = true;
+			int num;
+            while (true)
+			{
+				Console.WriteLine(num = int.Parse(Console.ReadLine().Split()[0]));
+				if (num == 42)
+				{
+					if (!checker)
+					{
+						counter++;
+						checker = true;
+					}
+					if (counter == 3)
+					{
+						return;
+					}
+				}
+				else
+				{
+					checker = false;
+				}
+			}
+		}
+	}
+}
