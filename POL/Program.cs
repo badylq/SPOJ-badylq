06c8607853cc6828797d903ce167c40c0b33a361
Krzysztof Zemlik <badylq@outlook.com>
2015-11-15 18:45:10 +0100

    POL

---
+++ POL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace POL
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			string inp;
+			for (int i = 0; i < t; i++)
+			{
+				inp = Console.ReadLine();
+				inp = inp.Remove(inp.Length/2);
+				Console.WriteLine(inp);
+			}
+		}
+	}
+}
