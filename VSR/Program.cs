a864fee8d3cd7806a0c0253727b618398742a419
Krzysztof Zemlik <badylq@outlook.com>
2015-11-15 18:57:18 +0100

    VSR

---
+++ VSR/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VSR
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] data = inp.Split(' ');
+				int v1 = int.Parse(data[0]);
+				int v2 = int.Parse(data[1]);
+                Console.WriteLine((2 * v1 * v2) / (v1 + v2));
+			}
+		}
+	}
+}
