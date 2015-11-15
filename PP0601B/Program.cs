4ed8da8dba5275f05d746464f86ce61b642628dc
Krzysztof Zemlik <badylq@outlook.com>
2015-11-15 19:16:52 +0100

    PP0601B

---
+++ PP0601B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PP0601B
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] data = inp.Split(' ');
+				int n = int.Parse(data[0]);
+				int x = int.Parse(data[1]);
+				int y = int.Parse(data[2]);
+				for (int j = 1; j < n; j++)
+				{
+					if (j%x == 0 && j%y != 0)
+					{
+						Console.Write(j + " ");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
