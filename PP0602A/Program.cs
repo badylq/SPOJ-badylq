57faa27ee01febfda146f43bbac0b418b7f3a295
Krzysztof Zemlik <badylq@outlook.com>
2015-11-15 23:10:58 +0100

    PP0602A

---
+++ PP0602A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PP0602A
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] data = inp.Split(' ');
+				int size = int.Parse(data[0]);
+				for (int j = 2; j < size+1; j += 2)
+				{
+					Console.Write(data[j] + " ");
+				}
+				for (int j = 1; j < size + 1; j += 2)
+				{
+					Console.Write(data[j] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
