6857c62a5759697e8dced61e467e3782373cc13e
Krzysztof Zemlik <badylq@outlook.com>
2015-11-17 11:05:48 +0100

    MWPZ06D

---
+++ MWPZ06D/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MWPZ06D
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				string[] data = inp.Split(' ');
+				int x = int.Parse(data[0]);
+				int y = int.Parse(data[1]);
+				if (x - 1 > y || x == 1)
+				{
+					Console.WriteLine("TAK");
+				}
+				else
+				{
+					if (y%(x-1) > 0)
+					{
+						Console.WriteLine("TAK");
+					}
+					else
+					{
+						Console.WriteLine("NIE");
+					}
+				}
+			}
+		}
+	}
+}
