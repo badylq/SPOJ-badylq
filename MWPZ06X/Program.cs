8139c85b72dd3bd33198ddb3089295e162810f6b
Krzysztof Zemlik <badylq@outlook.com>
2015-11-17 10:20:35 +0100

    MWPZ06X

---
+++ MWPZ06X/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MWPZ06X
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = int.Parse(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				int a = int.Parse(Console.ReadLine());
+				Console.WriteLine(a*a);
+			}
+		}
+	}
+}
