dd72865664be33bd563eb725d78339d6e05f4b62
Krzysztof Zemlik <badylq@outlook.com>
2015-11-18 14:08:16 +0100

    JROWLIN

---
+++ JROWLIN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace JROWLIN
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp = Console.ReadLine();
+			string[] data = inp.Split(' ');
+			double a = double.Parse(data[0]);
+			double b = double.Parse(data[1]);
+			double c = double.Parse(data[2]);
+			if (a == 0.0d)
+			{
+				if (b == c)
+				{
+					Console.WriteLine("NWR");
+				}
+				else
+				{
+					Console.WriteLine("BR");
+				}
+			}
+			else
+			{
+				double result = (c - b) / a;
+				Console.WriteLine("{0:0.00}", result);
+			}
+		}
+	}
+}
