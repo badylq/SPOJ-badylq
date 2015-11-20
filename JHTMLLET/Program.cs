621c693263ce8bedadb314f41bfe820c29f4e94d
Krzysztof Zemlik <badylq@outlook.com>
2015-11-20 22:51:15 +0100

    JHTMLLET

---
+++ JHTMLLET/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace JHTMLLET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			while (!string.IsNullOrEmpty(inp = Console.ReadLine()))
+			{
+				bool isTag = false;
+				foreach (var c in inp)
+				{
+					if (c == '<')
+					{
+						isTag = true;
+						Console.Write(c);
+					}
+					else
+					{
+						if (c == '>')
+						{
+							isTag = false;
+							Console.Write(c);
+						}
+						else
+						{
+							if (c >= 'A')
+							{
+								if (isTag && c >= 'a')
+								{
+									Console.Write((char)((int)c - 32));
+								}
+								else
+								{
+									Console.Write(c);
+								}
+							}
+							else
+							{
+								Console.Write(c);
+							}
+						}
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
