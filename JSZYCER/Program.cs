2cd8ff3dee35a38398c9ccb37f36076efc58c13c
Krzysztof Zemlik <krzysztofzemlik95@gmail.com>
2016-12-03 22:15:55 +0100

    WSEGA, POTSAM, JSZYCER, JLITOSL

---
+++ JSZYCER/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace JSZYCER
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StringBuilder inp = new StringBuilder(Console.ReadLine());
+			while (!String.IsNullOrWhiteSpace(inp.ToString()))
+			{
+				for (int i = 0; i < inp.Length; i++)
+				{
+					if(inp[i] == ' ')
+						continue;
+					inp[i] = (char) ((int) inp[i] > 87 ? (int)inp[i] - 23 : (int)inp[i] + 3);
+				}
+				Console.WriteLine(inp.ToString());
+				inp = new StringBuilder(Console.ReadLine());
+			}
+		}
+	}
+}
---
+++ WSEGA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSEGA
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int t = Convert.ToInt32(Console.ReadLine());
+			for (int i = 0; i < t; i++)
+			{
+				string inp = Console.ReadLine();
+				int n;
+				List<int> nums = inp.Split(' ').ToList().ConvertAll(Convert.ToInt32);
+				n = nums[0];
+				nums.RemoveAt(0);
+				Console.WriteLine(n-1 + nums.Sum());
+			}
+		}
+	}
+}
