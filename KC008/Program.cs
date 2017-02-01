29a463ca0199f5baedf7e0cf99c453e4d579893e
Krzysztof Zemlik <krzysztofzemlik95@gmail.com>
2017-02-01 20:50:27 +0100

    KC008

---
+++ KC008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace KC008
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			Int64 totalSum = 0;
+			inp = Console.ReadLine();
+			while (!String.IsNullOrWhiteSpace(inp))
+			{
+				Int64 sum = 0;
+				Int64[] nums = inp.Split(' ').ToList().ConvertAll(Convert.ToInt64).ToArray();
+				foreach (var num in nums)
+				{
+					sum += num;
+				}
+				Console.WriteLine(sum);
+				totalSum += sum;
+				inp = Console.ReadLine();
+			}
+			Console.WriteLine(totalSum);
+		}
+	}
+}
