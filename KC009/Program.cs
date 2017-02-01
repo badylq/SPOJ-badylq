b86be20ab1517c6fa0b46e2130443d0682af0d69
Krzysztof Zemlik <krzysztofzemlik95@gmail.com>
2017-02-01 20:53:25 +0100

    KC009

---
+++ KC009/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KC009
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inp;
+			inp = Console.ReadLine();
+			while (!String.IsNullOrWhiteSpace(inp))
+			{
+				for (int i = inp.Length-1; i >=0; i--)
+				{
+					Console.Write(inp[i]);
+				}
+				Console.WriteLine();
+				inp = Console.ReadLine();
+			}
+		}
+	}
+}
