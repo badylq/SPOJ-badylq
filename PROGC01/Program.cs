e0e7f57ee37f71f17a265e882225670d0147de62
Krzysztof Zemlik <krzysztofzemlik95@gmail.com>
2017-02-06 20:07:26 +0100

    PROGC01

---
+++ PROGC01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PROGC01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sum = 0;
+			string inp = Console.ReadLine();
+			while (inp != null)
+			{
+				++sum;
+				inp = Console.ReadLine();
+			}
+			Console.WriteLine(sum);
+		}
+	}
+}
